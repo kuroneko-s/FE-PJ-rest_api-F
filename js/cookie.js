bd0e1f25c45d1d79c6e28631f21a98a061f04340
kuron <drivespublic@gmail.com>
2020-11-22 19:25:34 +0900

    201122

---
+++ js/cookie.js
@@ -0,0 +1,15 @@
+var setCookie = function(name, value, day){
+    var date = new Date();
+    date.setTime(date.getTime() + day * 60 * 60 * 24 * 1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=../log/'
+}
+
+var getCookie = function(name){
+    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value? value[2] : null;
+}
+
+var deleteCookie = function(name) {
+    var date = new Date();
+    document.cookie = name + '= ;expirex=' + date.toUTCString() + ";path=../log/"
+}
